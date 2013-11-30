dbd53e1720f102963541b6269e731dd797f4e26d
Thomas Babu <tommy.developer@gmail.com>
2013-11-30 15:00:39 +0800

    added a view for new post page

--- second/app.js
+++ second/app.js
@@ -3,15 +3,19 @@
 
 var http = require('http');
 var url  = require('url');
+var fs  = require('fs');
+
+var newPostFormHTML = fs.readFileSync('views/post/new.html', 'UTF-8');
 
 function renderNewPostForm(request, response){
 
-	response.writeHead('200', {'Content-type' : 'text/plain' });
-	response.end('Hello, World - Blogging');
+	response.writeHead('200', {'Content-type' : 'text/html; charset=utf-8' });
+	response.end(newPostFormHTML);
 
 }
 
 function render404(request,response){
+	
 	response.writeHead(404);
 	response.end('404 File not found error');
 }
