cc8e87350af09e037fa47fd6c6cb16b8c740a6cd
Thomas Babu <tommy.developer@gmail.com>
2013-12-07 13:33:15 +0800

    Added a file to test node's callback

---
+++ first/callback.js
@@ -0,0 +1,19 @@
+// Testing Node Callbacks
+
+var myAsyncFunction = function( someArg1, someArg2, callback) {
+	// simulate some I/O was done
+	console.log ("Before setTimeout");
+
+	setTimeout( function(){
+		// 1 second latter, we are done with the I/O, call the callback
+		callback();
+	}, 1000);
+
+	console.log ("After setTimeout");
+};
+
+var callback = function() {
+	console.log ("I am in callback");
+}
+
+myAsyncFunction("arg1", "arg2", callback);
