24100c4eb017b14dd4199d6752fc3124ed074f40
Thomas Babu <tommy.developer@gmail.com>
2013-12-09 15:23:12 +0800

    addListener function

---
+++ examples/on.js
@@ -0,0 +1,15 @@
+/*
+    .addListener or .on
+*/
+
+//Get the fs module
+var fs = require("fs");
+
+var readStream = fs.createReadStream("/etc/passwd");
+readStream.on('data', function(data){
+	console.log(data/toString());
+});
+
+readStream.on('end', function(){
+	console.log('file ended');
+});
